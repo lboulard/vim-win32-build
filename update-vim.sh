e89670f6550217644e60bf8732d6409fc8a37c6a
Laurent Boulard <laurent.boulard@gmail.com>
2017-03-20 20:27:14 +0100

    Tag a new vim only when last commit is more than a few hours old

--- update-vim.sh
+++ update-vim.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+git submodule --quiet update --init --recursive
 git submodule --quiet foreach git pull -q origin master || exit
+if [ "$1" != "-f" ]; then
+	log=$(git -C vim log -1 --oneline --since="$(date -d '8 hour ago')" 2>/dev/null)
+	if test -n "$log"; then
+		echo "Last commit too recent: $log"
+		exit 0
+	fi
+fi
 git add vim
 if test -n "$(git diff-index --name-only HEAD --)" ; then
 	tag=$(git -C vim describe --tags 2>/dev/null)
