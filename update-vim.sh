ec798931147d8d640cc1e25ac511b99670f783e9
Laurent Boulard <laurent.boulard@gmail.com>
2017-03-20 20:35:38 +0100

    update-vim.sh exits with 0 only on new commit/tag

--- update-vim.sh
+++ update-vim.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-git submodule --quiet update --init --recursive
+git submodule --quiet update --init --recursive || exit
 git submodule --quiet foreach git pull -q origin master || exit
 if [ "$1" != "-f" ]; then
-	log=$(git -C vim log -1 --oneline --since="$(date -d '8 hour ago')" 2>/dev/null)
+	log=$(git -C vim log -1 --oneline --since="$(date -d '8 hour ago')" 2>/dev/null) || exit
 	if test -n "$log"; then
 		echo "Last commit too recent: $log"
-		exit 0
+		exit 1
 	fi
 fi
-git add vim
+git add vim || exit
 if test -n "$(git diff-index --name-only HEAD --)" ; then
 	tag=$(git -C vim describe --tags 2>/dev/null)
 	git commit -q -m "Vim update to $tag" && git tag $tag
+else
+	exit 1
 fi
