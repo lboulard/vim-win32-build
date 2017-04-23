282cf56c411de650e42facfb7520b4d61732604a
Laurent Boulard <laurent.boulard@gmail.com>
2017-04-23 09:59:31 +0200

    Strengthen error detection in update-vim.{bat,sh} scripts

--- update-vim.bat
+++ update-vim.bat
@@ -2,5 +2,5 @@
 git -C vim fetch || EXIT /B 1
 git -C vim checkout --detach origin/master || EXIT /B 1
 @FOR /F "delims=" %%i in ('git -C vim describe --tags') DO SET TAG=%%i
-git add -v vim
+git add -v vim || EXIT /B 1
 git commit -m "Vim update to %TAG%" && git tag %TAG%
--- update-vim.sh
+++ update-vim.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-git submodule --quiet update --init --recursive || exit
-git submodule --quiet foreach git pull -q --tags origin master || exit
+
+set -e
+
+git submodule --quiet update --init --recursive
+git submodule --quiet foreach git pull -q --tags origin master
 if [ "$1" != "-f" ]; then
-	log=$(git -C vim log -1 --oneline --since="$(date -d '5 hour ago')" 2>/dev/null) || exit
+	log=$(git -C vim log -1 --oneline --since="$(date -d '5 hour ago')" 2>/dev/null)
 	if test -n "$log"; then
 		echo "Last commit too recent: $(git -C vim log -1 --format="%ci, %cr"), $log"
 		exit 1
 	fi
 fi
-git add vim || exit
+git add vim
 if test -n "$(git diff-index --name-only HEAD --)" ; then
 	tag=$(git -C vim describe --tags 2>/dev/null)
 	git commit -q -m "Vim update to $tag" && git tag $tag
 else
 	exit 1
 fi
+exit 0
