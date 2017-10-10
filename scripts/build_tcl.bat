61781e44fcf07913b5ba1a196924060b8d6001fd
Laurent Boulard <laurent.boulard@gmail.com>
2017-10-10 23:42:11 +0200

    Reduce setup footprint of Tcl to save build time

--- scripts/build_tcl.bat
+++ scripts/build_tcl.bat
@@ -4,7 +4,7 @@
 @IF EXIST "%TCL_DIR%" RD /Q /S %TCL_DIR%
 @PUSHD %TCL_BUILD_DIR%\win || EXIT /B 1
 @ECHO ON
-nmake -nologo -f makefile.vc release || EXIT /B
-nmake -nologo -f makefile.vc install INSTALLDIR=%TCL_DIR% || EXIT /B
+nmake -nologo -f makefile.vc core shell dlls || EXIT /B
+nmake -nologo -f makefile.vc install-binaries install-libraries INSTALLDIR=%TCL_DIR% || EXIT /B
 @ECHO OFF
 @POPD
