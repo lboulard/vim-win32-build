23a156905fb974ed2ed584d7c09aaae41e45c484
Laurent Boulard <laurent.boulard@gmail.com>
2019-10-12 21:46:58 +0200

    configure: always uses related packages definition when running on AppVeyor

--- configure.bat
+++ configure.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
 @SETLOCAL
-IF "%APPVEYOR_REPO_TAG%" == "true" (
-SET VIMVER=%APPVEYOR_REPO_TAG_NAME%
+IF "%APPVEYOR%" == "True" (
 SET PACKAGES=packages.txt packages-appveyor.txt
 ) ELSE (
-FOR /F "delims=" %%i in ('git describe --tags --abbrev^=0') DO SET VIMVER=%%i
 SET PACKAGES=packages.txt packages-python.txt
+)
+IF "%APPVEYOR_REPO_TAG%" == "true" (
+SET VIMVER=%APPVEYOR_REPO_TAG_NAME%
+) ELSE (
+FOR /F "delims=" %%i in ('git describe --tags --abbrev^=0') DO SET VIMVER=%%i
 )
 @ECHO ON
 python.exe scripts\configure.py ^
