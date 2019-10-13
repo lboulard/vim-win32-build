e7e25449eeb7d2c8bbe7f0cd63750203f60c6178
Laurent Boulard <laurent.boulard@gmail.com>
2019-10-13 14:05:26 +0200

    gitlog: always return changes even in incomplete case
    Jump over non tagged changes due to build system modification and reports
    former to current tag for vim.
    If only one tag exists, report between HEAD and tag, or, if empty, reports
    from tag to root tree. When no tags was defined, report whole history.

--- scripts/gitlog.py
+++ scripts/gitlog.py
@@ -6,17 +6,31 @@ from subprocess import Popen, PIPE
 
 PATCHRE = re.compile(r'patch ([\.a-z0-9_-]+)[ ]*:[ ]*(.*)', flags=re.IGNORECASE)
 
-def previoustag():
-    # git describe --tag --abbrev=0 HEAD^
-    cmd=['git', 'describe', '--tag', '--abbrev=0', 'HEAD^']
+def root():
+    cmd = ["git", "hash-object", "-t", "tree", "/dev/null"]
     r = b''
     with Popen(cmd, stdout=PIPE, stderr=sys.stderr) as proc:
         r += proc.stdout.read()
     return r.decode().strip()
 
-def gitlog(fromtag):
+def gettag(ref="HEAD"):
+    cmd = ["git", "describe", "--exact-match", "--tags", ref]
+    r = b''
+    with Popen(cmd, stdout=PIPE, stderr=sys.stderr) as proc:
+        r += proc.stdout.read()
+    return r.decode().strip()
+
+def previoustag(ref="HEAD^"):
+    # git describe --tag --abbrev=0 HEAD^
+    cmd=['git', 'describe', '--tag', '--abbrev=0', ref]
+    r = b''
+    with Popen(cmd, stdout=PIPE, stderr=sys.stderr) as proc:
+        r += proc.stdout.read()
+    return r.decode().strip()
+
+def gitlog(fromtag, head='HEAD'):
     # git -C vim log --format=%s %PREVIOUSTAG%..HEAD
-    cmd=['git', '-C', 'vim', 'log', '--format=%s', fromtag + '..HEAD']
+    cmd=['git', '-C', 'vim', 'log', '--format=%s', fromtag + '..' + head]
     r = b''
     with Popen(cmd, stdout=PIPE, stderr=sys.stderr) as proc:
         r += proc.stdout.read()
@@ -34,12 +48,27 @@ def transform(line):
     else:
         return "* " + line
 
+def findcurrenttag():
+    ref, n ="HEAD", 0
+    tag = gettag()
+    while not tag and n < 50:
+        n = n + 1
+        tag = gettag(ref + "~" + str(n))
+    return tag
+
 def main():
-    fromtag = previoustag()
-    if fromtag:
-        log = gitlog(fromtag)
+    if len(sys.argv) > 1:
+        head = sys.argv[1]
+        tag = gettag(head)
+    else:
+        tag = findcurrenttag() or 'HEAD'
+    if tag:
+        fromtag = previoustag(tag + "^") or root()
+        log = gitlog(fromtag, tag)
         j = [transform(l) for l in log.splitlines()]
         print('\\n'.join(j))
+    else:
+        print('\\n')
 
 if __name__ == '__main__':
     main()
