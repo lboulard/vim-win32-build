a6e2c44f932e0d05a54c4bcda419e1c615b01d62
Laurent Boulard <laurent.boulard@gmail.com>
2021-07-07 21:50:47 +0200

    Remove VS2017 workaround in perl package build

--- scripts/build_perl.bat
+++ scripts/build_perl.bat
@@ -12,8 +12,6 @@ SET ARGS=INST_TOP=%PERL_DIR% CCTYPE=MSVC141
 IF "%ARCH%" == "x86" (
  SET ARGS=!ARGS! WIN64=undef
 )
-@REM Next line avoid a bug with setargv.obj in VS2017/Windows 10
-SET ARGS=!ARGS! CFG=DebugFull
 dmake -P%NUMBER_OF_PROCESSORS% -f makefile.mk %ARGS% all installbare || EXIT /B 1
 @ECHO OFF
 @POPD
